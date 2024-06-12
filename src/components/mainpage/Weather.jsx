e7dc2e61cc52ce9c479de4f6f0cdbefe7b3670ed
alstjr998 <alstjr998@naver.com>
2024-06-12 11:22:40 +0900

    weatherapi 작동 성공

--- src/components/mainpage/Weather.jsx
+++ src/components/mainpage/Weather.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import WeatherBox from './WeatherBox';
 
 const Weather = () => {
 
@@ -19,43 +20,6 @@ const Weather = () => {
     color: 'rgba(0, 0, 0, 0.46)',
   };
 
-  //날씨 API
-  const API_KEY = import.meta.env.VITE_WHEATER_API_KEY;
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      let lat = position.coords.latitude;
-      let lon = position.coords.longitude;
-      getWeather(lat, lon);
-    });
-  }, []);
-
-  const getWeather = async(lat, lon) => {
-    try {
-      const res = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-      );
- 
-      // id 찾아서 매칭 후 description 한글 번역된 거 가져오기
-      const weatherId = res.data.weather[0].id;
-      const weatherKo = weatherDescKo[weatherId];
-      // 날씨 아이콘 가져오기
-      const weatherIcon = res.data.weather[0].icon;
-      const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
-      // 소수점 버리기
-      const temp = Math.round(res.data.main.temp);
- 
-      setWeather({
-        description: weatherKo,
-        name: cityName,
-        temp: temp,
-        icon: weatherIconAdrs,
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   return (
     <div id="weather">
       <div id="text">
@@ -65,9 +29,7 @@ const Weather = () => {
       <div id="plusButton_weather" className="plusButton">
         <a href="https://weather.naver.com/">+더보기</a>
       </div>
-      <a href="https://weather.naver.com/" target="_blank" rel="noopener noreferrer">
-        <img src="/img/weather.png" alt="Weather" />
-      </a>
+      <WeatherBox />
     </div>
   );
 };
---
+++ src/components/mainpage/WeatherBox.jsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import weatherDescKo from './weatherDescKo';
+
+const WeatherBox = () => {
+
+  const [weather, setWeather] =useState(null);
+
+  //날씨 API
+  const API_KEY = import.meta.env.VITE_WHEATER_API_KEY;
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeather(lat, lon);
+    });
+  }, []);
+
+  const getWeather = async(lat, lon) => {
+    try {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+      );
+
+      const weatherId = res.data.weather[0].id;
+      const weatherKo = weatherDescKo[weatherId];
+
+      //날씨 아이콘 가져오기
+      const weatherIcon = res.data.weather[0].icon;
+      const weatherIconAdrress = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+
+      const cityName = res.data.name;
+
+      const temp = Math.round(res.data.main.temp);
+ 
+      setWeather({
+        description: weatherKo,
+        name: cityName,
+        temp: temp,
+        icon: weatherIconAdrress,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return(
+    <div className="weather-box">
+      <div>{weather?.name}</div>
+      <h2>
+        {weather?.temp}℃
+      </h2>
+      <h3>{weather?.description}</h3>
+    </div>
+  );
+};
+
+export default WeatherBox;
