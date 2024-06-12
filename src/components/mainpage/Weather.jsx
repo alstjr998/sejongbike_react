cd263bda3367e86bf60fa2bbc2aa25326d1a1730
alstjr998 <alstjr998@naver.com>
2024-06-12 10:40:35 +0900

    날씨 함수 추가 및 수정중(weather branch)

--- src/components/mainpage/Weather.jsx
+++ src/components/mainpage/Weather.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 
 const Weather = () => {
+
+  // 오늘의 날짜 표시
   const [today, setToday] = useState('');
 
   useEffect(() => {
@@ -15,6 +17,43 @@ const Weather = () => {
     fontSize: '13px',
     fontWeight: '700',
     color: 'rgba(0, 0, 0, 0.46)',
+  };
+
+  //날씨 API
+  const API_KEY = import.meta.env.VITE_WHEATER_API_KEY;
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeather(lat, lon);
+    });
+  }, []);
+
+  const getWeather = async(lat, lon) => {
+    try {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+      );
+ 
+      // id 찾아서 매칭 후 description 한글 번역된 거 가져오기
+      const weatherId = res.data.weather[0].id;
+      const weatherKo = weatherDescKo[weatherId];
+      // 날씨 아이콘 가져오기
+      const weatherIcon = res.data.weather[0].icon;
+      const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+      // 소수점 버리기
+      const temp = Math.round(res.data.main.temp);
+ 
+      setWeather({
+        description: weatherKo,
+        name: cityName,
+        temp: temp,
+        icon: weatherIconAdrs,
+      });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
