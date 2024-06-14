6a45a8aa147ffdd08b250b36bf4230721891c648
alstjr998 <alstjr998@naver.com>
2024-06-14 16:27:24 +0900

    서비스 안내 페이지 제작 시작

---
+++ src/ServiceInfo.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const ServiceInfo = () => {
+  return(
+    <>
+      <Header />
+      
+      <Footer />
+    </>
+  );
+};
+
+export default ServiceInfo;
