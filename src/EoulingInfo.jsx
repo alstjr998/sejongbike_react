e9d71025007bdc3e81d8992f262641e412f794be
alstjr998 <alstjr998@naver.com>
2024-06-20 12:39:20 +0900

    반응형 페이지 드롭다운 메뉴 컴포넌트 추가

--- src/EoulingInfo.jsx
+++ src/EoulingInfo.jsx
@@ -1,12 +1,16 @@
+import DropdownMenuEouling from "./components/DropdownMenuEouling";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import InfoContent from "./components/eoulinginfopage/InfoContent";
 
 const EoulingInfo = () => {
 
+  const pageName = "어울링소개";
+
   return(
     <>
       <Header />
+      <DropdownMenuEouling pageName={pageName} />
       <InfoContent />
       <Footer />
     </>
