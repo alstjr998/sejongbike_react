ae0da24e7af7e0e3eafc7b687e359568013fb848
alstjr998 <alstjr998@naver.com>
2024-06-20 14:13:38 +0900

    반응형 페이지 네비게이션 바 추가

--- src/Duty.jsx
+++ src/Duty.jsx
@@ -1,11 +1,16 @@
+import DropdownMenuEouling from "./components/DropdownMenuEouling";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import DutyContent from "./components/dutypage/DutyContent";
 
 const Duty = () => {
+
+  const pageName = "의무와 책임"
+
   return(
     <>
       <Header />
+      <DropdownMenuEouling pageName={pageName} />
       <DutyContent />
       <Footer />
     </>
--- src/Insurance.jsx
+++ src/Insurance.jsx
@@ -1,12 +1,16 @@
+import DropdownMenuEouling from "./components/DropdownMenuEouling";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import InsuranceContent from "./components/insurancepage/InsuranceContent";
 
 const Insurance = () => {
 
+  const pageName = "시민자전거보험";
+
   return(
     <>
       <Header />
+      <DropdownMenuEouling pageName={pageName} />
       <InsuranceContent />
       <Footer />
     </>
