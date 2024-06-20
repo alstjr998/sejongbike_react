86335415dec63327a381b0fc51f5e0a55d42ea87
alstjr998 <alstjr998@naver.com>
2024-06-20 12:13:21 +0900

    반응형 페이지 내비게이션 메뉴 추가

--- src/ServiceInfo.jsx
+++ src/ServiceInfo.jsx
@@ -1,19 +1,38 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ServiceInfoContent from "./components/serviceinfopage/ServiceInfoContent";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const ServiceInfo = () => {
+
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const dropdownToggle = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   return(
     <>
       <Header />
-      
+
       <div className="responsiveMenu">
         <div id="responsiveMenuTitle">
           <p>어울링이란</p>
         </div>
-        <div id="responsiveMenuDropdown">
-          <p>서비스안내</p>
-          <p id="dropdownBtn">▶</p>
+        <div id="responsiveMenuDropdown" onClick={dropdownToggle}>
+          <p style={{width: "80%"}}>서비스안내</p>
+          <p className={dropdownVisible ? 'dropdownBtn active' : 'dropdownBtn'}>▶</p>
+          <nav id="dropdownMenuList" className={dropdownVisible ? 'dropdown active' : 'dropdown'}>
+            <ul>
+              <li><Link to="/eoulinginfo">어울링소개</Link></li>
+              <li><Link to="/serviceinfo">서비스안내</Link></li>
+              <li><Link to="/duty">의무와 책임</Link></li>
+              <li><Link to="/insurance">시민자전거보험</Link></li>
+              <li><Link to="/app">어울링 앱 소개</Link></li>
+              <li><a href="https://www.sejongbike.kr/usage/bikeUsage">자전거이용정보</a></li>
+            </ul>
+          </nav>
         </div>
       </div>
 
