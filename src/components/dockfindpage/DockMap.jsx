f09cb34f98f68200c54c6ec81358515df2a19fe7
alstjr998 <alstjr998@naver.com>
2024-06-21 16:28:00 +0900

    지도 마킹 작업

--- src/components/dockfindpage/DockMap.jsx
+++ src/components/dockfindpage/DockMap.jsx
@@ -1,7 +1,15 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { Container as Mapdiv, Marker, NaverMap, useNavermaps } from "react-naver-maps";
 
 const DockMap = () => {
+
+
+  const dockApiKey = import.meta.env.VITE_DOCK_API_KEY;
+
+  const [loading, setLoading] = useState(false);
+
+  const [dock, setDock] = useState([]);
 
   const navermaps = useNavermaps();
 
@@ -13,16 +21,50 @@ const DockMap = () => {
       setLat(position.coords.latitude);
       setLon(position.coords.longitude);
     });
+    fetchDock();
   }, []);
+
+  const fetchDock = async () => {
+    setLoading(true);
+    try{
+      const response =  await axios.get(
+        `https://api.odcloud.kr/api/15091421/v1/uddi:8f04ee4a-dc5d-47ec-b421-f8c72d2f12ad?page=1&perPage=614&serviceKey=${dockApiKey}`
+      );
+
+      const docks = response.data.data.map((item) => ({
+        dockLat: item['경도'],
+        dockLon: item['위도'],
+        dockName: item['대여소 이름'],
+        dockNum: item['대여소 번호'],
+        dockLocation: item['주소(동)'],
+      }));
+
+      setDock(docks);
+
+    } catch (error) {
+      console.error("위치 정보를 받아오는데 실패했습니다.", error);
+    }
+    setLoading(false);
+  };
 
   //상태가 업데이트 된 후 지도 렌더
   if (lat === null || lon === null) {
     return <div>Loading...</div>;
   }
-  
+
   return(
     <Mapdiv style={{width: "100%", height: "700px", marginTop: "15px"}}>
       <NaverMap defaultCenter={new navermaps.LatLng(lat, lon)} defaultZoom={17}>
+        {dock.length === 0 ? (
+          <p>API 조회 불가</p>
+          ) : (
+            <div>
+              {dock.map((dockList, index) => (
+                <Marker key={index} position={new navermaps.LatLng(dockList.dockLat, dockList.dockLon)} />
+              ))}
+            </div>
+          )
+        }
       </NaverMap>
     </Mapdiv>
   );
