f7cb89e36c94c3018902666879a75b7e8aabbf99
alstjr998 <alstjr998@naver.com>
2024-07-05 12:26:37 +0900

    필요없는 로직 삭제

--- src/util/axiosWithoutAuth.js
+++ src/util/axiosWithoutAuth.js
@@ -13,21 +13,8 @@ const axiosWithOutAuth = async (url, method = 'GET', data = null, options = {}) 
       },
     });
 
-    // 응답 헤더에서 새로운 토큰 확인
-    const newToken = response.headers['authorization'];
-    if (newToken) {
-      // 'Bearer ' 접두사 제거 후 새로 저장
-      const cleanToken = newToken.replace('Bearer ', '');
-      localStorage.setItem("accessToken", cleanToken);
-    }
-
     return response;
   } catch (error) {
-    // 토큰이 유효하지 않은 경우 로그인 페이지로 리디렉션
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem("accessToken");
-      window.location.href = "/login";
-    }
     return Promise.reject(error);
   }
 };
