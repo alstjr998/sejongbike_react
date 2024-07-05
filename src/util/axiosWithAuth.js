f2cf80084f7eb2989205dd88d448805309a2e185
alstjr998 <alstjr998@naver.com>
2024-07-05 12:28:11 +0900

    토큰 만료로 인한 401 응답시 refreshToken 엔드포인트 진입 및 발급

--- src/util/axiosWithAuth.js
+++ src/util/axiosWithAuth.js
@@ -13,6 +13,7 @@ const axiosWithAuth = async (url, method = 'GET', data = null, options = {}) => 
     const response = await axios({
       url: url,
       method: method,
+      withCredentials: true,
       data: method === 'POST' ? data : null,
       ...options,
       headers: {
@@ -22,7 +23,7 @@ const axiosWithAuth = async (url, method = 'GET', data = null, options = {}) => 
     });
 
     // 응답 헤더에서 새로운 토큰 확인
-    const newToken = response.headers['authorization'];
+    const newToken = response.headers['Authorization'];
     if (newToken) {
       // 'Bearer ' 접두사 제거 후 새로 저장
       const cleanToken = newToken.replace('Bearer ', '');
@@ -31,11 +32,38 @@ const axiosWithAuth = async (url, method = 'GET', data = null, options = {}) => 
 
     return response;
   } catch (error) {
-    // 토큰이 유효하지 않은 경우 로그인 페이지로 리디렉션
+    // 토큰이 유효하지 않은 경우 리프레시 토큰을 통해 새로운 액세스 토큰을 요청
     if (error.response && error.response.status === 401) {
-      localStorage.removeItem("accessToken");
-      window.location.href = "/login";
+      try {
+        const refreshResponse = await axios.post('http://localhost:8080/refresh-token', null, {
+          withCredentials: true,
+        });
+
+        // 새로운 액세스 토큰 저장
+        const newAccessToken = refreshResponse.data.accessToken;
+        localStorage.setItem("accessToken", newAccessToken);
+
+        // 원래 요청을 새로운 액세스 토큰으로 다시 시도
+        const retryResponse = await axios({
+          url: url,
+          method: method,
+          data: method === 'POST' ? data : null,
+          ...options,
+          headers: {
+            ...options.headers,
+            Authorization: `Bearer ${newAccessToken}`,
+          },
+        });
+
+        return retryResponse;
+      } catch (refreshError) {
+        // 리프레시 토큰도 유효하지 않은 경우 로그인 페이지로 리디렉션
+        localStorage.removeItem("accessToken");
+        window.location.href = "/login";
+        return Promise.reject(refreshError);
+      }
     }
+
     return Promise.reject(error);
   }
 };
